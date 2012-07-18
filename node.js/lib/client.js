ab8a0969cb62ede42b540dd206b6d520a049fe4e
indexzero <charlie.robbins@gmail.com>
2012-07-18 08:54:13 -0400

    [refactor] Ensure `.createClient()` only returns resources for each respective web-service

--- node.js/lib/client.js
+++ node.js/lib/client.js
@@ -13,13 +13,23 @@ exports.Systems = require('./client/systems').Systems;
 exports.Servers = require('./client/servers').Servers;
 exports.Users   = require('./client/users').Users;
 
-exports.createClient = function (options) {
-  return {
-    config: new exports.Config(options),
-    groups: new exports.Groups(options),
-    roles: new exports.Roles(options),
-    systems: new exports.Systems(options),
-    servers: new exports.Servers(options),
-    users: new exports.Users(options)
-  };
+exports.createClient = function (type, options) {
+  if (type === 'composer') {
+    return {
+      config:  new exports.Config(options),
+      systems: new exports.Systems(options),
+      users:   new exports.Users(options)
+    };
+  }
+  else if (type === 'provisioner') {
+    return {
+      groups:  new exports.Groups(options),
+      roles:   new exports.Roles(options),
+      servers: new exports.Servers(options),
+      users:   new exports.Users(options)
+    };
+  }
+  else {
+    throw new Error('Unknown client type');
+  }
 };

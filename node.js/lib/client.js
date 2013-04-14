85e1d3a3d624dae80400e117a7ce7527b345a656
Maciej Małecki <me@mmalecki.com>
2013-04-14 16:23:35 +0200

    [refactor] Remove `composer-api` from `conservatory-api`

--- node.js/lib/client.js
+++ node.js/lib/client.js
@@ -6,30 +6,16 @@
  */
  
 exports.Client  = require('./client/client').Client;
-exports.Config  = require('./client/config').Config;
 exports.Groups  = require('./client/groups').Groups;
 exports.Roles   = require('./client/roles').Roles;
-exports.Systems = require('./client/systems').Systems;
 exports.Servers = require('./client/servers').Servers;
 exports.Users   = require('./client/users').Users;
 
-exports.createClient = function (type, options) {
-  if (type === 'composer') {
-    return {
-      config:  new exports.Config(options),
-      systems: new exports.Systems(options),
-      users:   new exports.Users(options)
-    };
-  }
-  else if (type === 'provisioner') {
-    return {
-      groups:  new exports.Groups(options),
-      roles:   new exports.Roles(options),
-      servers: new exports.Servers(options),
-      users:   new exports.Users(options)
-    };
-  }
-  else {
-    throw new Error('Unknown client type');
-  }
+exports.createClient = function (options) {
+  return {
+    groups:  new exports.Groups(options),
+    roles:   new exports.Roles(options),
+    servers: new exports.Servers(options),
+    users:   new exports.Users(options)
+  };
 };
